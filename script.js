9821eedeaf2e68790ace840af6fcd498752c7325
starved-hippo <69477482+starved-hippo@users.noreply.github.com>
2024-04-12 23:30:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,6 @@ $(document).ready(function () {
 
 	$.getJSON("messages.json", function (json) {
 		$(".msg").text(json["cute_greeting_messages"][Math.floor(Math.random() * json["cute_greeting_messages"].length)]);
-		$(".msg").text(currenttime.getTime() - starttime);
 	});
 
 	$(".dropdown-item").click(function (e) {
